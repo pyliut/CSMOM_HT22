ac84a5a0faf79833516c581b5dffc8ddba21eab7
oh54321 <38576852+oh54321@users.noreply.github.com>
2022-03-10 12:14:49 +0000

    Changed to correct model

--- ML_OliverHayman.py
+++ ML_OliverHayman.py
@@ -12,70 +12,71 @@ import sys
 from sklearn.linear_model import LinearRegression
 from xgboost import XGBRegressor
 
-yrStep = 5
-minReq = 20
-def predictTo(model, ticker, ind1, ind2):
-  X = featureDic[ticker]
-  y = returnDic[ticker]
-  X_train = X[X.index <= ind1].set_index("month")
-  y_train = y[y.index <= ind1].set_index("month")
-  X_test = X[(X.index <= ind2) & (X.index > ind1)].set_index("month")
-  y_test = y[(y.index <= ind2) & (y.index > ind1)].set_index("month")
-  model.fit(X_train, y_train)
-  return model.predict(X_test)
-def getPreds(model, ticker, pred):
-  X = featureDic[ticker]
-  y = returnDic[ticker]
-  if len(X) < minReq:
-    return
-  firstDate = X["month"][minReq-2]
-  lastDate = X["month"][len(X["month"])-1]
-  year = int(firstDate[0:4])
-  month = firstDate[5:7]
-  monthCol = list(X["month"])
-  index
-  while index:
-    date1 = str(year)+"-"+month
-    date2 = str(year+yrStep)+"-"+month
-    if compare(date2, lastDate) >= 0:
-      date2 = lastDate
-      endOfData = True
-    pred["pred"] += [float(x[0]) for x in list(predictTo(model, ticker, date1, date2))]
-    year += yrStep
-  mnthList = list(X["month"][X["month"].index > minReq-2])
-  pred["TICKER"] += [ticker]*len(mnthList)
-  pred["month"] += mnthList
-def compare(date1, date2): #returns 1 if date 1 is after date 2, -1 if date 2 after date 1, 0 otherwise
-  #dates are of the form YYYY-MM
-  year1 = int(date1[0:4])
-  year2 = int(date2[0:4])
-  month1 = int(date1[5:7])
-  month2 = int(date2[5:7])
-  if year1 > year2:
-    return 1
-  elif year1 < year2:
-    return -1
-  elif month1 > month2:
-    return 1
-  elif month1 < month2:
-    return -1
-  else:
-    return 0
+
+
+
+
+
+yrStep = 5 #Number of years in each prediction period
 df_NN = pd.read_csv("df_NeuralNetworkFeatures.csv")
-df_NN = df_NN[["TICKER","month","pred_target","debt_assets","de_ratio","evm","pe_exi","roe","npm","ps","ptb","pcf","aftret_invcapx","CPI","FedFundsTargetRate","GDP","MedianHomeSalesPrice","NonFarmPayrolls","PMI","PPI","PrivateHousingStarts","Unemployment","MEDPTG","PCTUP4W","PCTDOWN4W","volume"]]#,"price_adjusted"]]
-tickers = df_NN["TICKER"].squeeze().unique()
-featureDic = {tk:df_NN[df_NN.TICKER == tk].reset_index().drop(["TICKER", "pred_target", "index"],1) for tk in tickers}
-returnDic = {tk:df_NN[df_NN.TICKER == tk][["pred_target", "month"]].reset_index().drop(["index"],1) for tk in tickers}
-pred = {"TICKER":[], "month":[],"pred":[]} 
-for tk in tickers:
-  getPreds(LinearRegression(), tk, pred)
-df_Ret = pd.DataFrame(data = pred).set_index("month")
-predictionDic = {tk:df_Ret[df_Ret.TICKER == tk].drop(["TICKER"],1) for tk in tickers}
-for tk in tickers:
-  returnDic[tk].set_index("month")
-predictionDic[tickers[0]].plot()
-returnDic[tickers[0]].plot()
+df_NN = df_NN[["PERMNO","month","pred_target","debt_assets","de_ratio","evm","pe_exi","roe","npm","ps","ptb","pcf","aftret_invcapx","CPI","FedFundsTargetRate","GDP","MedianHomeSalesPrice","NonFarmPayrolls","PMI","PPI","PrivateHousingStarts","Unemployment","MEDPTG","PCTUP4W","PCTDOWN4W","volume"]]#,"price_adjusted"]]
+permnos = df_NN["PERMNO"].squeeze().unique() #List of PERMNOs
+dates = [] #Dates will be an array of arrays, each element will be a list of dates that will be predicted together on the same model. 
+for i in range(2000, 2022, yrStep): 
+  dates.append([])
+  for j in range(i, i+yrStep): #Loops through each year in a prediction period
+    for k in range(1, 13): #Loops through each month
+      dates[len(dates)-1].append(str(j)+"-"+f"{k:02}") #Adds them to one element
+print("Dates in prediction periods: " + str(dates))
 
 
 
 
+
+
+df_split = [] #df_split will be an array of dataframes, each dataframe will contain the data from a [yrstep]-year period
+for dt in dates:
+  df_split.append(df_NN[df_NN["month"].isin(dt)].set_index("month")) 
+df_train = [] #df_train[i] will contain all of the data before the period of df_split[i], which will be used to train its model
+for i in range(len(dates)):
+  if i == 0:
+    df_train.append(None)
+  else:
+    df_train.append(pd.concat([df_split[j] for j in range(len(df_split)) if j < i]).drop("PERMNO", 1))
+
+
+
+
+
+
+
+def getPreds(model, train_data, test_data): #Gets predictions for one [yrStep]-year period
+  y_test = train_data["pred_target"]
+  X_test = train_data.drop("pred_target", 1)
+  X = test_data.drop("pred_target", 1)
+  model.fit(X_test, y_test)
+  permnos = list(test_data["PERMNO"])
+  permno_dic = {pn:test_data[test_data["PERMNO"] == pn].drop(["PERMNO", "pred_target"], 1) for pn in permnos} #We separate out the data for each PERMNO we will test, which allows us to drop them when plugging in the data to the model and add the PERMNOs back in after
+  preds = []
+  for pn in permno_dic: #Loops through each PERMNO and creates a dataframe for prediction data, then concatenates all of these dataframes
+    df = pd.DataFrame({'month': permno_dic[pn].index, 'PERMNO': [pn]*len(permno_dic[pn]), 'pred_target': pd.Series(model.predict(permno_dic[pn]))})
+    preds.append(df)
+  return pd.concat(preds)
+def predict(model, train, test): #Loops through each [yrStep]-year period and gets predictions, then concatenates these predictions to one large dataframe
+  preds = []
+  for i in range(1, len(train)):
+    preds.append(getPreds(model, train[i], test[i]))  
+  return pd.concat(preds).set_index("month")
+
+df_preds = predict(XGBRegressor(), df_train, df_split)
+
+
+
+
+
+def plot(pn, df_p): #Plots the predicted and actual returns for a PERMNO pn, where df_p is dataframe of predictions
+  df_predicted = df_preds[df_p["PERMNO"] == pn].rename(columns = {"pred_target":"Predicted Returns"})["Predicted Returns"]
+  df_measured = df_NN[df_NN["PERMNO"] == pn].set_index("month").rename(columns = {"pred_target":"Actual Returns"})["Actual Returns"]
+  df_combined = pd.concat([df_predicted, df_measured], axis = 1, sort = True)
+  df_combined.plot()
+plot(permnos[2], df_preds)
